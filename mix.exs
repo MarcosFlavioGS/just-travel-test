f2aadb5a7ca36e0821f646f6dd756366035bc7ed
Marcos Flavio <mfghost69@gmail.com>
2025-12-19 13:28:32 -0300

    chore: update package version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule JustTravelTest.MixProject do
   def project do
     [
       app: :just_travel_test,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.19.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
